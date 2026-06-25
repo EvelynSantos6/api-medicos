7872bc6fca9538e979c997abd8d8266d41b66324
EvelynSantos6 <evelyn.santos20032@gmail.com>
2026-06-24 23:16:06 -0300

    Criação endpoints

---
+++ src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -0,0 +1,4 @@
+package med.voll.api.medico;
+
+public record DadosCadastroMedico(String nome, String email, Integer crm, Especialidade especialidade, Endereco endereco) {
+}
---
+++ src/main/java/med/voll/api/medico/Endereco.java
@@ -0,0 +1,5 @@
+package med.voll.api.medico;
+
+public record Endereco (String logradouro, String bairro, Integer cep, String cidade, String uf, Integer numero, String complemento) {
+
+}
